f8f4926955227ed04bb966c242d9af7eace0d8ac
Jyoti Panda <you@example.com>
2025-04-13 23:07:06 +0530

    for production

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: 'https://seat-booking-system-lac.vercel.app/', credentials: true }));
 app.use(express.json());
 
 // Routes
